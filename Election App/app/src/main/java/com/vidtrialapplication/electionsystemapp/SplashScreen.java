6df7bb3000e9b5308bb2c4d21845bf2919d8aea9
Wen Gu <guw@sunyit.edu>
2015-03-14 16:58:06 -0400

    Renamed folder. Reading on authentication method.

---
+++ Election App/app/src/main/java/com/vidtrialapplication/electionsystemapp/SplashScreen.java
@@ -0,0 +1,53 @@
+package com.vidtrialapplication.electionsystemapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class SplashScreen extends ActionBarActivity {
+
+    //Duration of wait
+    private final static int SPLASH_SCREEN_DURATION = 1000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent loginActivity = new Intent(SplashScreen.this, LoginActivity.class);
+                SplashScreen.this.startActivity(loginActivity);
+                SplashScreen.this.finish();
+            }
+        }, SPLASH_SCREEN_DURATION);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_splash_screen, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
